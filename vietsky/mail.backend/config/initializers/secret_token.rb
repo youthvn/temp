52de9f7753b40dc9b1149172be4222fef67c2ca4
shinlogin <shinlogin@ubuntu.ubuntu-domain>
2012-11-13 02:47:18 -0500

    Save

---
+++ vietsky/mail.backend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailBackend::Application.config.secret_token = 'b3c28e4d3aa3db11eb56b53852a7c4692afb5bea0237832e41c334ecc45f841f6f508eea43b4295bf82cd7ded50b69205a0c110fe5ec3ba115fadc384d036fa5'
